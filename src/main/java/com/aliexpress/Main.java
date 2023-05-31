32acf8ebf1a23c748ddf8507842828b4b7ced78f
dolma <dolmatova-nata@mail.ru>
2023-05-31 15:39:35 +0300

    change logic

--- src/main/java/com/aliexpress/Main.java
+++ src/main/java/com/aliexpress/Main.java
@@ -2,7 +2,8 @@ package com.aliexpress;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, qa_guru!!!");
+        if (args.length == 0) {
+            System.out.println("Hello, qa_guru!!!");
+        }
     }
 }
