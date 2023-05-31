c0880ec5f01c7a387c1c656624d56ac01b098ff4
dolma <dolmatova-nata@mail.ru>
2023-05-31 16:21:13 +0300

    change message

--- src/main/java/com/aliexpress/Main.java
+++ src/main/java/com/aliexpress/Main.java
@@ -3,7 +3,7 @@ package com.aliexpress;
 public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("message");
+            System.out.println("message!!!");
         }
     }
 }
